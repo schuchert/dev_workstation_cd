a6ed0c340263c501558402891d13ce0da80aa2e0
Brett L. Schuchert <schuchert@yahoo.com>
2017-02-07 22:58:20 -0500

    set root user password for mariadb to "vagrant"

--- cookbooks/mariadb/recipes/default.rb
+++ cookbooks/mariadb/recipes/default.rb
@@ -1,8 +1,11 @@
 bash 'install mariadb' do
   code <<-EOH
-  	apt-get install software-properties-common
-		add-apt-repository 'deb http://mirror.jmu.edu/pub/mariadb/repo/10.1/ubuntu trusty main'
-		apt-get -qq update
-		yes y | apt-get install mariadb-server
-	EOH
+    export DEBIAN_FRONTEND=noninteractive
+    debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password password vagrant'
+    debconf-set-selections <<< 'mariadb-server-10.1 mysql-server/root_password_again password vagrant'
+    apt-get install software-properties-common
+    add-apt-repository 'deb http://mirror.jmu.edu/pub/mariadb/repo/10.1/ubuntu trusty main'
+    apt-get -qq update
+    apt-get -y --force-yes install mariadb-server
+  EOH
 end
